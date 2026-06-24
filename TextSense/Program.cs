76cc9766a169a569b4917229f6362351cb45c5be
Kamil Piróg <kamilpirog98@gmail.com>
2026-06-24 14:08:06 +0200

    feat: register AI services and typed HTTP clients

--- TextSense/Program.cs
+++ TextSense/Program.cs
@@ -1,9 +1,38 @@
+using System.Net.Http.Headers;
+using Microsoft.Extensions.Options;
 using TextSense.Components;
+using TextSense.Configuration;
+using TextSense.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.Configure<OpenAiOptions>(
+    builder.Configuration.GetSection(OpenAiOptions.SectionName));
+builder.Services.Configure<AzureLanguageOptions>(
+    builder.Configuration.GetSection(AzureLanguageOptions.SectionName));
+
+builder.Services.AddHttpClient(OpenAiTextGenerationService.HttpClientName, (sp, client) =>
+{
+    var options = sp.GetRequiredService<IOptions<OpenAiOptions>>().Value;
+    if (!string.IsNullOrWhiteSpace(options.Endpoint))
+        client.BaseAddress = new Uri(options.Endpoint);
+    client.DefaultRequestHeaders.Authorization =
+        new AuthenticationHeaderValue("Bearer", options.ApiKey);
+});
+
+builder.Services.AddHttpClient(AzureTextAnalysisService.HttpClientName, (sp, client) =>
+{
+    var options = sp.GetRequiredService<IOptions<AzureLanguageOptions>>().Value;
+    if (!string.IsNullOrWhiteSpace(options.Endpoint))
+        client.BaseAddress = new Uri(options.Endpoint);
+    client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", options.ApiKey);
+});
+
+builder.Services.AddScoped<ITextGenerationService, OpenAiTextGenerationService>();
+builder.Services.AddScoped<ITextAnalysisService, AzureTextAnalysisService>();
 
 var app = builder.Build();
 
